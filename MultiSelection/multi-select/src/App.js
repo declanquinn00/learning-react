ef7edf972489253d73eea5626aef6fa022f69c9b
Declan Quinn <98410755+declanquinn00@users.noreply.github.com>
2024-04-23 19:48:50 +0100

    Project 1: Single and Multi Selection

---
+++ MultiSelection/multi-select/src/App.js
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+import './App.css';
+import Accordian from './components/accordian'
+
+function App() {
+  return (
+    <div classname="App">
+      {/* Accordian component*/}
+      <Accordian/>
+    </div>
+  );
+}
+
+export default App;
