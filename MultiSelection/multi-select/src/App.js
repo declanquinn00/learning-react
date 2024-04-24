4ef5de39403ab85cc3ed24b30809f61612a823eb
Declan Quinn <98410755+declanquinn00@users.noreply.github.com>
2024-04-24 13:17:53 +0100

    project 4 images with slider options

--- MultiSelection/multi-select/src/App.js
+++ MultiSelection/multi-select/src/App.js
@@ -3,6 +3,7 @@ import './App.css';
 import Accordian from './components/accordian'
 import RandomColor from './components/random-color';
 import StarRating from './components/star-rating';
+import ImageSlider from './components/image-slider';
 
 function App() {
   return (
@@ -15,6 +16,9 @@ function App() {
 
       {/* Star Rating Component */}
       <StarRating/>
+
+      {/*Image Slider Component (url from freecodecamp)*/}
+      <ImageSlider url={'https://picsum.photos/v2/list'} page = {"1"} limit={'10'}/>
     </div>
   );
 }
