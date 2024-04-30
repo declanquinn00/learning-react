6cfe257dd68c0b6f460e5c8a1ab95ad673e09540
Declan Quinn <98410755+declanquinn00@users.noreply.github.com>
2024-04-30 18:12:44 +0100

    project 6 nested tree menu

--- MultiSelection/multi-select/src/App.js
+++ MultiSelection/multi-select/src/App.js
@@ -5,6 +5,8 @@ import RandomColor from './components/random-color';
 import StarRating from './components/star-rating';
 import ImageSlider from './components/image-slider';
 import LoadMoreData from './components/load-more-data';
+import TreeView from './components/tree-view';
+import menus from "./components/tree-view/data";
 
 function App() {
   return (
@@ -23,6 +25,9 @@ function App() {
 
       {/*Load more products Component*/}
       <LoadMoreData></LoadMoreData>
+
+      {/*Treeview component*/}
+      <TreeView menus={menus}/>
     </div>
   );
 }
