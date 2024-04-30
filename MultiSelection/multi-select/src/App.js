70ed2fa1372191aed743960d88efcfbf0a8895d9
Declan Quinn <98410755+declanquinn00@users.noreply.github.com>
2024-04-30 16:07:35 +0100

    project 5 loading data

--- MultiSelection/multi-select/src/App.js
+++ MultiSelection/multi-select/src/App.js
@@ -4,6 +4,7 @@ import Accordian from './components/accordian'
 import RandomColor from './components/random-color';
 import StarRating from './components/star-rating';
 import ImageSlider from './components/image-slider';
+import LoadMoreData from './components/load-more-data';
 
 function App() {
   return (
@@ -19,6 +20,9 @@ function App() {
 
       {/*Image Slider Component (url from freecodecamp)*/}
       <ImageSlider url={'https://picsum.photos/v2/list'} page = {"1"} limit={'10'}/>
+
+      {/*Load more products Component*/}
+      <LoadMoreData></LoadMoreData>
     </div>
   );
 }
