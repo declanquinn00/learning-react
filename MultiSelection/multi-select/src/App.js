a1d70352c7dface9ace111bcccf0bbbcd1081c9b
Declan Quinn <98410755+declanquinn00@users.noreply.github.com>
2024-05-18 13:40:15 +0100

    Tic tac Toe

--- MultiSelection/multi-select/src/App.js
+++ MultiSelection/multi-select/src/App.js
@@ -14,6 +14,7 @@ import TabTest from './components/tabs/tab-test';
 import ModalTest from './components/custom-modal-popup/modal-test';
 import GithubProfileFinder from './components/github-profile-finder';
 import SearchAutoComplete from './components/search-autocomplete';
+import TicTacToe from './components/tic-tac-toe';
 
 function App() {
   return (
@@ -57,6 +58,9 @@ function App() {
 
       {/*Search Autocomplete*/}
       <SearchAutoComplete/>
+
+      {/* Tic Tac Toe*/}
+      <TicTacToe/>
 
     </div>
   );
